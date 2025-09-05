82c01b420cc5968a41a925488fc91a7da666a044
Mario Rugiero <mrugiero@gmail.com>
2025-09-05 10:29:58 -0300

    bump solc requirement

--- crates/l2/contracts/src/l1/interfaces/IOnChainProposer.sol
+++ crates/l2/contracts/src/l1/interfaces/IOnChainProposer.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity =0.8.29;
+pragma solidity =0.8.30;
 
 /// @title Interface for the OnChainProposer contract.
 /// @author LambdaClass
--- crates/l2/contracts/src/l2/L2ToL1Messenger.sol
+++ crates/l2/contracts/src/l2/L2ToL1Messenger.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity =0.8.29;
+pragma solidity =0.8.30;
 
 import "./interfaces/IL2ToL1Messenger.sol";
 
